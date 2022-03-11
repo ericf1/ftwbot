c932aa9dffe1e570dd4e9f443dc92f3b22ac3812
Eric <efang@wustl.edu>
2022-03-10 23:58:42 -0600

    TwitterAPI updated

---
+++ twitterAPI.py
@@ -0,0 +1,26 @@
+#Load the dotenv library and then the dotenv file
+from dotenv import load_dotenv 
+load_dotenv()
+
+import os
+import tweepy
+
+TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
+TWITTER_API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY')
+TWITTER_ACCESS_KEY = os.getenv('TWITTER_ACCESS_KEY')
+TWITTER_ACCESS_SECRET_TOKEN = os.getenv('TWITTER_ACCESS_SECRET_TOKEN')
+
+#Authentication object
+authenticate = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET_KEY)
+
+#Access token and Access Token secret
+authenticate.set_access_token(TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET_TOKEN)
+
+#API object to pass in auth info
+api = tweepy.API(authenticate, wait_on_rate_limit = True)
+def latestTweet(username):
+    posts = api.user_timeline(screen_name=f"{username}", count=1, tweet_mode="extended")
+    postId = posts[0].id
+    return f"https://twitter.com/{username}/status/{postId}"
+
+print(latestTweet('elonmusk'))
